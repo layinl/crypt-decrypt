037aba57ed1ead7a64f1ec8ac16330028881b779
layinl <layon.costa@protonmail.com>
2024-03-14 22:24:27 -0300

    feat: fix textarea to work properly and adjust minor spacings

--- app.js
+++ app.js
@@ -62,3 +62,11 @@ function checkIfEmpty(text) {
   }
   return false;
 }
+
+let textarea = document.querySelector(".textarea");
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
