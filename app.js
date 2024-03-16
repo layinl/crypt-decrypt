bba5a25701b87547861af87269e491402b94c0e3
layinl <layon.costa@protonmail.com>
2024-03-15 23:09:14 -0300

    feat: perfected checkSpecialChars for real this time!

--- app.js
+++ app.js
@@ -78,7 +78,8 @@ function checkIfEmpty(text) {
 }
 
 function checkIfSpecialChar(text) {
-  if(!text.match(/[a-z]|\s/g)) {
+  text = text.replace(/\s/g, "");
+  if(text.match(/\W|_/g)) {
     outputTitle.removeAttribute("hidden");
     outputButtonCopy.setAttribute("hidden", "");
     outputTitle.innerText = "Mensagem inválida";
