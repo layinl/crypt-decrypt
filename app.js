a9e3172e9e4c7baafa4d6916290d2e9c04bb59cc
layinl <layon.costa@protonmail.com>
2024-03-19 22:22:05 -0300

    docs: document app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let textarea = document.querySelector(".textarea");
+textarea.addEventListener('input', autoResize, false);
 let outputImg = document.querySelector(".output-img");
 let output = document.querySelector(".output-text");
 let outputMessage = output.innerText;
@@ -6,7 +8,7 @@ let outputTitleMessage = outputTitle.innerText;
 let outputButtonCopy = document.querySelector(".output-button-copy");
 
 /**
- * Encrypts the text in .output-text
+ * Encrypts the text in .output-text if valid or not empty
  * @returns the encryptedText
  */
 function encrypt() {
@@ -42,7 +44,7 @@ function encrypt() {
 }
 
 /**
- * Decrypts the text in .output-text
+ * Decrypts the text in .output-text if valid or not empty
  * @returns the decryptedText
  */
 function decrypt() {
@@ -62,8 +64,10 @@ function decrypt() {
 }
 
 /**
- * Checks if text is empty and toggles visibility in the .output section elements
+ * Checks if text is empty, toggles visibility in the .output section elements and changes its text to default
+ * @param {string} text the text to be checked
  * @returns true if text is empty, false otherwise
+ * @see toggleOutputVisibility
  */
 function checkIfEmpty(text) {
   if(text === "") {
@@ -75,19 +79,21 @@ function checkIfEmpty(text) {
   return false;
 }
 
+/**
+ * 
+ * @param {string} text the text to be checked
+ * @returns true if text contains a non lowercase letter (except number), false otherwise
+ */
 function checkIfSpecialChar(text) {
   text = text.replace(/\s/g, "");
   if(text.match(/\W|[A-Z]|_/g)) {
     toggleOutputVisibility(false);
     outputTitle.innerText = "Mensagem inválida";
-    output.innerText = "Digite um texto sem letras com acento ou sem caracteres especiais.";
+    output.innerText = "Digite um texto sem letras maiúsculas, sem letras acentuadas ou sem caracteres especiais.";
     return true;
   }
   return false;
 }
-
-let textarea = document.querySelector(".textarea");
-textarea.addEventListener('input', autoResize, false);
 
 /**
  * Automatically resizes the element based on input
@@ -97,13 +103,21 @@ function autoResize() {
     this.style.height = this.scrollHeight + 'px';
 }
 
-
+/**
+ * Copies the output text to clipboard
+ */
 function copyOutput() {
   navigator.clipboard.writeText(output.innerText);
   outputButtonCopy.innerHTML = "Copiado!";
   setTimeout(() => outputButtonCopy.innerHTML = "Copiar", 3000);
 }
 
+/**
+ * toggles the output section visibility based on whether the input text is valid
+ * - Shows the copy button, hides image and title if isTextValid is true
+ * - Hides the copy button, shows image and title if isTextValid is false
+ * @param {boolean} isTextValid if text is valid
+ */
 function toggleOutputVisibility(isTextValid = true) {
   if(isTextValid) {
     outputImg.setAttribute("hidden", "");
